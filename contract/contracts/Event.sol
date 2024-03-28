4c76fa6106d2828582f4b70c1dea1ab111e18ade
duncankuo <blood680@gmail.com>
2024-03-28 16:28:25 +0800

    add function and deploy

--- contract/contracts/Event.sol
+++ contract/contracts/Event.sol
@@ -4,19 +4,87 @@ pragma solidity >=0.6.0 <0.9.0;
 
 contract Event {
  address public manager;
- address payable[] public players;
+ string public eventName;
+ uint public dueDate;
+ Option[] public options;
+ Player[] public players;
 
-   constructor() {
+   struct Option {
+      uint id;
+      string name;
+      uint256 count;
+   }
+   
+   struct Player {
+       address account;
+       uint256 price;
+       uint selection;
+   }
+
+   constructor(string memory name, string[] memory optionNames, uint due) {
        manager = msg.sender;
+       eventName = name;
+       dueDate = due;
+       for (uint i = 0 ; i < optionNames.length; i++) {
+          options.push(Option({
+            id: i,
+            name: optionNames[i],
+            count: 0
+          }));
+       }
    }
 
-   function enter() public payable {
+   function enter(uint selection) public payable {
        require(msg.value > 0.01 ether);
-       players.push(payable(msg.sender));
+       Player memory player = Player({
+        account: msg.sender,
+        price: msg.value,
+        selection: selection
+       });
+       players.push(player);
+
+       for (uint i = 0 ; i < options.length ; i++) {
+          if (options[i].id == selection) {
+            options[i].count++;
+          }
+       }
    }
 
-   function getPlayers() public view returns (address payable[] memory) {
-       return players;
+   function getProfile() public view returns (string memory, uint, Option[] memory, Player[] memory) {
+       return (eventName, dueDate, options, players);
+   }
+
+   function getWinnerCount(uint selection) public view returns (uint256) {
+      //統計贏家
+      uint256 winnerCount = 0;
+      for (uint i = 0 ; i < players.length ; i++) {
+        if (players[i].selection == selection) {
+            winnerCount++;
+        }
+      }
+      return winnerCount;
+   }
+
+   function getTotalPrice() public view returns (uint256) {
+      //取得總獎金額
+      uint256 total = 0;
+      for (uint i = 0 ; i < players.length ; i++) {
+        total = total + players[i].price;
+      } 
+      return total;
+   }
+
+    function endEvent(uint selection) public {
+        require(msg.sender == manager);
+        uint256 winnerCount = getWinnerCount(selection);
+        uint256 total = getTotalPrice();
+        uint256 reward = total / winnerCount;
+
+        for (uint i = 0 ; i < players.length ; i++) {
+            if (players[i].selection == selection) {
+                payable(players[i].account).transfer(reward);
+            }
+        }
    }
    
 }
--- contract/migrations/1_initial_migration.js
+++ contract/migrations/1_initial_migration.js
@@ -1,6 +1,5 @@
 const Event = artifacts.require("Event");
 
-
 module.exports = function (deployer) {
-  deployer.deploy(Event);
+  deployer.deploy(Event, 'no1', ['a','b','c'], 1711435654);
 };
